6e2d499ee9edee6f015d14907b2dcd07e93ec51e
Suraj Gupta Gudla <surajgupta268@gmail.com>
2021-05-26 12:14:56 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "student.c5ylf3yirenb.us-east-2.rds.amazonaws.com"
+customuser = "aarmanvu"
+custompass = "thinkbig1995"
+customdb = "student"
+custombucket = "addstudent101"
+customregion = "us-east-2"
